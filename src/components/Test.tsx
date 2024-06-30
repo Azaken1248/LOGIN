646318f59845ec62a4f7d37c8e6c7062853309f0
Azaken1248 <suvkrm@gmail.com>
2024-06-30 13:11:41 +0530

    initial commit

---
+++ src/components/Test.tsx
@@ -0,0 +1,13 @@
+const Test: React.FC = () => {
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <h1 className="text-4xl font-bold text-blue-600">
+          Hello, Tailwind CSS with React and TypeScript!
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default Test;
